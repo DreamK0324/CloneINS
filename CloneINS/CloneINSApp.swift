28c665bc9fdf3a64ad0ade1ec7669a706b119fd0
dreamk0324 <dreamk0324@gmail.com>
2023-08-07 18:56:41 -0400

    Initial Commit

---
+++ CloneINS/CloneINSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloneINSApp.swift
+//  CloneINS
+//
+//  Created by Dream K on 8/7/23.
+//
+
+import SwiftUI
+
+@main
+struct CloneINSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
